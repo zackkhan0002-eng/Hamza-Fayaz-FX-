1145ba25b6145aab9d536ab7bfdccfa6b41eb18f
Hamza Fayaz <zack.khan0002@gmail.com>
2026-04-06 19:24:04 +0500

    Update discipline.js

--- discipline.js
+++ discipline.js
@@ -2,13 +2,11 @@ document.getElementById('save-discipline').onclick = function() {
     const btn = this;
     const msg = document.getElementById('save-msg');
     
-    // Date select nahi ki to aaj ki date uthayega
     let dateVal = document.getElementById('disc-date').value;
     if(!dateVal) {
         dateVal = new Date().toLocaleDateString();
     }
 
-    // Data collect karna
     const data = {
         date: dateVal,
         cme: document.getElementById('chk-cme').checked ? "YES" : "NO",
@@ -25,27 +23,24 @@ document.getElementById('save-discipline').onclick = function() {
     btn.innerText = "SAVING TO VAULT...";
     btn.disabled = true;
 
-    // Aapka Latest Web App URL yahan update kar diya hai
-    fetch('https://script.google.com/macros/s/AKfycbwi8npRFpmd_zE8-_ZH-yY1wnohxLqUS901xqLYJDKORpfYHOLu4i2fiZkbOtJy7d1aJw/exec', {
+    // Aapka New Validated Link
+    fetch('https://script.google.com/macros/s/AKfycbwLbOYMkUfacZVylGe7KijQST5hxELewo6JVL66at8ciZN5tAAdIJoBAQBG0-hZN5AmDA/exec', {
         method: 'POST',
         mode: 'no-cors', 
         cache: 'no-cache',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
     }).then(() => {
-        // Success message
         btn.innerText = "SAVE DISCIPLINE";
         btn.disabled = false;
         msg.classList.remove('hidden');
         setTimeout(() => msg.classList.add('hidden'), 3000);
         
-        // Checkboxes reset
         const checkboxes = document.querySelectorAll('input[type="checkbox"]');
         checkboxes.forEach(cb => cb.checked = false);
     }).catch(err => {
-        alert("Connection Error! Check internet.");
+        alert("Connection Error!");
         btn.disabled = false;
         btn.innerText = "SAVE DISCIPLINE";
     });
 };
-    
